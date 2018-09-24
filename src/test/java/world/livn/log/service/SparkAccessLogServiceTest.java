988811301003a65f0034054bd0ec8b0d8f08d5d8
Peng Wang <peng.wang@livn.world>
2018-09-24 16:43:16 +1000

    aa

--- src/test/java/world/livn/log/service/SparkAccessLogServiceTest.java
+++ src/test/java/world/livn/log/service/SparkAccessLogServiceTest.java
@@ -1,6 +1,7 @@
 package world.livn.log.service;
 
-import org.apache.spark.sql.AnalysisException;
+import java.util.regex.Pattern;
+
 import org.apache.spark.sql.SparkSession;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -13,19 +14,22 @@ public class SparkAccessLogServiceTest {
 
 	@BeforeAll
 	public static void setUpBeforeClass() throws Exception {
-		spark = SparkSession.builder().appName("Java Spark SQL basic example").config("spark.master", "local[*]")
-				.getOrCreate();
+		//		spark = SparkSession.builder().appName("Java Spark SQL basic example").config("spark.master", "local[*]")
+		//				.getOrCreate();
 
 	}
 
 	@Test
 	public void testRunBasicDataFrameExample() {
 
-		try {
-			SparkAccessLogService.runBasicDataFrameExample(this.spark);
-		} catch (AnalysisException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		String str="IP: 192.155.83.114 , Host Name: gds1.livngds.com , Login Name: livn , Internal API User Host=192.155.83.114, Roles=[WEBFRONT, HAL, GDS, CMS] , Geolocation: US United States: Fremont 37.5483,-121.9886";
+
+		Pattern pattern = Pattern.compile("^[a-z]*[:|=][A-Za-z0-9\\.\\:\\,]$");
+		String [] split =pattern.split(str);
+		System.out.println(split.toString());
+		for (String string : split) {
+			System.out.println(string);
 		}
 	}
 
