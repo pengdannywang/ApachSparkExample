2834d693242a5fdc2d53253e5117dbe7e5e63546
Peng Wang <peng.wang@livn.world>
2018-10-03 16:47:18 +1000

    a

--- src/test/java/world/livn/log/service/SparkAccessLogServiceTest.java
+++ src/test/java/world/livn/log/service/SparkAccessLogServiceTest.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 
-@RunWith(JUnitPlatform.class)
+
+
+
 public class SparkAccessLogServiceTest {
 	// static SparkSession spark;
 
@@ -22,6 +23,7 @@ public class SparkAccessLogServiceTest {
 	}
 
 	@Test
+	@Ignore
 	public void testRunBasicDataFrameExample() {
 
 		String str = "IP: 192.155.83.114 , Host Name: gds1.livngds.com , Login Name: livn , Internal API User Host=192.155.83.114, Roles=[WEBFRONT, HAL, GDS, CMS] , Geolocation: US United States: Fremont 37.5483,-121.9886\n";
@@ -48,5 +50,14 @@ public class SparkAccessLogServiceTest {
 		}
 
 	}
-
+	@Test
+	public void testSplitDate(){
+		String test="?startDate=2018-09-06&endDate=2018-09-12";
+		Pattern pattern =Pattern.compile("\\?startDate=|&endDate=");
+		String[] splits = test.split(pattern.pattern());
+		System.out.println(splits.length);
+		for(String s:splits){
+			System.out.println(s);
+		}
+	}
 }
